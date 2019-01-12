e0c115a50fa3c05c314abe978d81ee992fd23031
jamessykwan <jamessykwan@gmail.com>
2019-01-12 13:50:22 -0800

    armmotor

---
+++ src/main/cpp/Subsystems/ArmMotor.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Subsystems/ArmMotor.h"
+
+ArmMotor::ArmMotor() : Subsystem("ExampleSubsystem") {}
+
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	TalonSRX* armMotor;
+	//Encoder* armEncoder;
+	double maxPosition;
+	double minPosition;
+	DigitalInput* lowLimit;
+	Counter* counter;
+  
+void ArmMotor::InitDefaultCommand() {
+  // Set the default command for a subsystem here.
+  // SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
